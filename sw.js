e8ee512058e431999b8226ad91c3911bcb4d7e3e
MyTravelManager2026 <nomamiki112099@gmail.com>
2026-09-15 18:22:44 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const APP_VERSION = 'v37';
-const CACHE_NAME = 'mytravel-cache-v37';
+const APP_VERSION = 'v38';
+const CACHE_NAME = 'mytravel-cache-v39';
 const APP_SHELL = ['./My%20Travel.html', './manifest.webmanifest'];
-const EXTERNAL_CACHE = 'mytravel-external-v37';
+const EXTERNAL_CACHE = 'mytravel-external-v39';
 
 self.addEventListener('install', event => {
   event.waitUntil(
